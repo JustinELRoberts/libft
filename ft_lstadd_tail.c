e9547680e367a897c7ea7a2f97703ee331001413
Justin Roberts <jroberts@e1z3r3p25.42.us.org>
2020-03-01 13:33:08 -0800

    New version

--- ft_lstadd_tail.c
+++ ft_lstadd_tail.c
@@ -6,7 +6,7 @@
 /*   By: jroberts <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/25 12:34:57 by jroberts          #+#    #+#             */
-/*   Updated: 2020/02/25 12:36:42 by jroberts         ###   ########.fr       */
+/*   Updated: 2020/02/27 13:32:48 by jroberts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,11 @@ void	ft_lstadd_tail(t_list **alst, t_list *new_lst)
 	t_list		*link;
 
 	link = *alst;
+	if (link == NULL)
+	{
+		*alst = new_lst;
+		return ;
+	}
 	while (link->next != NULL)
 		link = link->next;
 	link->next = new_lst;
