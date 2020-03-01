1eeafb6af0702465f309541f71ac4e85285837a2
Justin Roberts <jroberts@e1z3r3p25.42.us.org>
2020-03-01 13:27:22 -0800

    Reverting to older version

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: jroberts <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/25 10:57:56 by jroberts          #+#    #+#             */
-/*   Updated: 2020/02/27 14:52:19 by jroberts         ###   ########.fr       */
+/*   Updated: 2020/02/25 17:17:14 by jroberts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
 {
 	del((*alst)->content, (*alst)->content_size);
-	free((*alst)->content);
 	free(*alst);
 	*alst = NULL;
 }
--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: jroberts <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/25 10:26:54 by jroberts          #+#    #+#             */
-/*   Updated: 2020/02/27 15:02:28 by jroberts         ###   ########.fr       */
+/*   Updated: 2020/03/01 13:27:05 by jroberts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,7 @@ t_list		*ft_lstnew(const void *content, size_t content_size)
 
 	result = (t_list*)malloc(sizeof(t_list));
 	if (result == NULL)
-	{
-		free(result);
 		return (NULL);
-	}
 	result->content = (void*)content;
 	if (content == NULL)
 		result->content_size = 0;
