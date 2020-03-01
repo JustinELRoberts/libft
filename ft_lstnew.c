be56c173a75481c281635f22be3ada87e60abedd
Justin Roberts <jroberts@e1z3r3p25.42.us.org>
2020-03-01 13:44:36 -0800

    Final change

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: jroberts <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/25 10:26:54 by jroberts          #+#    #+#             */
-/*   Updated: 2020/02/27 15:02:28 by jroberts         ###   ########.fr       */
+/*   Updated: 2020/03/01 13:42:16 by jroberts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,9 @@ t_list		*ft_lstnew(const void *content, size_t content_size)
 {
 	t_list	*result;
 
-	result = (t_list*)malloc(sizeof(t_list));
+	result = (t_list*)ft_memalloc(sizeof(t_list));
 	if (result == NULL)
-	{
-		free(result);
 		return (NULL);
-	}
 	result->content = (void*)content;
 	if (content == NULL)
 		result->content_size = 0;
